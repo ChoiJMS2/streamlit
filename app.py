ca961351e5866504ad8f6cd43661f3acd8ab5131
ChoiJMS2 <cjm881209@gmail.com>
2023-04-24 10:17:05 +0900

    update

--- app.py
+++ app.py
@@ -7,3 +7,32 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+    # text
+    st.text('This is so {}'.format("good"))
+
+    # Header
+    st.header('This is subHeader')
+
+    # Subheader
+    st.subheader('This is Markdown')
+
+    # Markdown
+    st.markdown('## This is Markdown')
+
+    # 색상이 들어간 텍스트 feature
+    st.success('성공')
+    st.warning('경고')
+    st.info('정보와 관련된 탭')
+    st.error('에러 메시지')
+    st.exception('예외 처리')
+
+    # st.write()
+    st.write('일반 텍스트')
+    st.write(1+2)
+    st.write(dir(str))
+
+    st.title(':sunglasses:')
+
+    # Help
+    st.help(range)
